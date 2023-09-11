cc83f44ea863168fd27c9603ef964acd3b5eded2
Connie Leung <cleung334@gmail.com>
2023-09-11 22:31:27 +0800

    feat: signal rxjs-interop demo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,14 +2,16 @@ import { Component } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 import { CombineLatestCounterComponent } from './combine-latest-counter/combine-latest-counter.component';
 import { RxjsControlPanelComponent } from './rxjs-control-panel/rxjs-control-panel.component';
+import { SignalControlPanelComponent } from './signal-control-panel/signal-control-panel.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CombineLatestCounterComponent, RxjsControlPanelComponent],
+  imports: [CombineLatestCounterComponent, RxjsControlPanelComponent, SignalControlPanelComponent],
   template: `
     <div>
       <app-rxjs-control-panel></app-rxjs-control-panel>
+      <app-signal-control-panel></app-signal-control-panel>
       <app-combine-latest-counter></app-combine-latest-counter>
     </div>
   `,
--- src/app/signal-control-panel/signal-control-panel.component.ts
+++ src/app/signal-control-panel/signal-control-panel.component.ts
@@ -1,13 +1,80 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component, computed, signal } from '@angular/core';
+import { NgFor } from '@angular/common';
+import { SpeedUpPipe, getTextColor } from '../core';
+import { toSignal, toObservable } from '@angular/core/rxjs-interop';
+import { pairwise } from 'rxjs';
 
 @Component({
   selector: 'app-signal-control-panel',
   standalone: true,
-  imports: [CommonModule],
-  templateUrl: './signal-control-panel.component.html',
-  styleUrls: ['./signal-control-panel.component.scss']
+  imports: [NgFor, SpeedUpPipe],
+  template: `
+  <h1>Signal RxJS Interop Demo</h1>
+    <div>
+      <span>Speed:</span>
+      <button *ngFor="let value of [5, 10, 30, 50];trackBy: trackByIndex" (click)="speedSignal.set(value)">{{ value }}</button>
+    </div>
+    <div>
+      <span>Skip:</span>
+      <button *ngFor="let value of [-30, -15, 10, 20];trackBy: trackByIndex" (click)="calculateSkip(value)">{{ value }}</button>
+    </div>
+    <div>
+      <span>Volume:</span>
+      <button (click)="adjustVolume(-1)">-</button>
+      <button (click)="adjustVolume(1)">+</button>
+    </div>
+    <ul>
+      <li>Current Time: <span [style.color]="skipStyle()">{{ skipSignal() }}</span></li>
+      <li>Speed: <span [style.color]="speedStyle()">{{ speedPair() | speedUp }}</span></li>
+      <li>Volume: <span>{{ volSignal() }}</span></li>
+    </ul>
+  `,
+  styles: [`
+    :host {
+      display: block;
+    }
+
+    div {
+      padding: 0.5rem;
+    }
+
+    button {
+      margin-right: 0.25rem;
+      padding: 0.25rem;
+    }
+
+    ul {
+      padding: 1.5rem;
+    }
+  `],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class SignalControlPanelComponent {
+  speedSignal = signal(0, { equal: () => false });
+  skipSignal = signal(0);
+  volSignal = signal(0);
 
+  speedPair = toSignal(
+    toObservable(this.speedSignal).pipe(pairwise()), 
+    { initialValue: [0, 0] as [number, number] }
+  );
+
+  speedStyle = computed(() => { 
+    const [prev = 0, curr] = this.speedPair() || [];
+    return getTextColor(curr - prev);
+  });
+
+  skipStyle = computed(() => getTextColor(this.skipSignal()));
+
+  trackByIndex(index: number): number {
+    return index;
+  }
+
+  adjustVolume(delta: number) {
+    this.volSignal.update((v) => v + delta);
+  }
+
+  calculateSkip(delta: number) {
+    this.skipSignal.update((s) => s + delta);
+  }
 }
